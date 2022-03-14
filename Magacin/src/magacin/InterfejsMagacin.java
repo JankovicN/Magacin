b3617f9d58245a6cd2b5826fdced00f1b34e27ac
JankovicN <nj20180199@student.fon.bg.ac.rs>
2022-03-14 15:26:04 +0100

    Kreiran paket magacin, interfejs InterfejsMagacin i unutar njega metode za dodavanje, izbacivanje i pronalazenje artikla

---
+++ Magacin/src/magacin/InterfejsMagacin.java
@@ -0,0 +1,11 @@
+package magacin;
+
+import artikli.Artikal;
+
+public interface InterfejsMagacin {
+
+	public void dodajArtikal(Artikal artikal);
+	public void izbaciArtikal(Artikal artikal);
+	public Artikal pronadjiArtikal(int sifra);
+	
+}

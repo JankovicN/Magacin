5d95ab96aab0f503208e986cdf5f0be50cc1f0f9
JankovicN <nj20180199@student.fon.bg.ac.rs>
2022-03-14 15:41:33 +0100

    Napravljena klasa Magacin koja implementira interfejs InterfejsMagacin

---
+++ Magacin/src/magacin/Magacin.java
@@ -0,0 +1,25 @@
+package magacin;
+
+import artikli.Artikal;
+
+public class Magacin implements InterfejsMagacin {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

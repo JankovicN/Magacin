aca365b41eaff76a2f13a609b26310e5cfa45283
JankovicN <nj20180199@student.fon.bg.ac.rs>
2022-03-14 15:45:01 +0100

    Implementacija metoda klase Magacin

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,24 +1,36 @@
 package magacin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import artikli.Artikal;
 
 public class Magacin implements InterfejsMagacin {
 
+	List<Artikal> artikli = new ArrayList<>();
+	
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if (artikal!=null) {
+			artikli.add(artikal);
+		}
 
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikal!=null) {
+			artikli.remove(artikal);
+		}
 
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
+		for (Artikal a : artikli) {
+			if(a.getSifra()==sifra)
+				return a;
+		}
 		return null;
 	}
 
